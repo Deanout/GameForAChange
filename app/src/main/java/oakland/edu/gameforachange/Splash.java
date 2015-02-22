1073c252da84e0a1d34e9d4d2f7144f44c88e226
Dean <dddehart@oakland.edu>
2015-02-22 04:53:52 -0500

    Splash Screen
    Activities renamed, splash screen working, logo changed.

---
+++ app/src/main/java/oakland/edu/gameforachange/Splash.java
@@ -0,0 +1,39 @@
+package oakland.edu.gameforachange;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * Created by Dean on 2/22/2015.
+ */
+public class Splash extends Activity implements View.OnClickListener {
+    public ImageView proceedButton;
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splash);
+
+        proceedButton = (ImageView)findViewById(R.id.splashLogo);
+        proceedButton.setOnClickListener(this);
+    }
+
+
+
+    private void splashLogoClick() {
+        startActivity(new Intent("oakland.edu.gameforachange.MainMenu"));
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.splashLogo:
+                splashLogoClick();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/oakland/edu/gameforachange/UserSelectionMenu.java
@@ -0,0 +1,14 @@
+package oakland.edu.gameforachange;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Dean on 2/21/2015.
+ */
+public class UserSelectionMenu extends Activity {
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+    }
+}
